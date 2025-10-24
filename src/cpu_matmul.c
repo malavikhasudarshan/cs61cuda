37f7dc2f0c6af161ab9d8836372fd7e9a8115e5b
Malavikha Sudarshan <malavikhasudarshan@berkeley.edu>
2025-10-24 10:40:58 -0700

    [remove] sol

--- src/cpu_matmul.c
+++ src/cpu_matmul.c
@@ -1,12 +1,5 @@
 #include <iostream>
 
 void cpu_matmul(const float *A, const float *B, float *C, int m, int n, int k) {
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            float acc = 0.f;
-            for (int p = 0; p < k; p++)
-                acc += A[i * k + p] * B[p * n + j];
-            C[i * n + j] = acc;
-        }
-    }
+    //TODO: Implement CPU matrix multiplication
 }
